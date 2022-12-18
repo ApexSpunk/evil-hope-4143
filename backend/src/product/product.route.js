4b14339de7b1f7ea0d375b1fe1c19671eadc429b
imrangadwal10 <imrangadwal777@gmail.com>
2022-12-18 17:27:41 +0530

    searchbar

--- backend/src/product/product.route.js
+++ backend/src/product/product.route.js
@@ -17,6 +17,15 @@ app.get("/", async (req, res) => {
     }
 });
 
+app.get("/search", async (req, res) => {
+    try {
+        const products = await Product.find()
+        res.status(200).send(products);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+});
+
 app.get("/:id", async (req, res) => {
     try {
         const product = await Product.findById(req.params.id);
