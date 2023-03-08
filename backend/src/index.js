217462847e27349b82da93cc568303ea2b8bce86
imrangadwal10 <imrangadwal777@gmail.com>
2023-03-08 18:25:20 +0530

    added login signup

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,13 +9,8 @@ const userRoute = require("./user/user.route")
 const productRoute = require("./product/product.route")
 const cartRoute = require("./cart/cart.route")
 
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json())
 
 app.use('/user', userRoute)
