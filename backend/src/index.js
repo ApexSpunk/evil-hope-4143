ee3e26a6f737124ebd69aaf3df2c872ccda0bc94
imrangadwal10 <imrangadwal777@gmail.com>
2023-03-08 16:21:24 +0530

    added login signup

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,13 +5,17 @@ const connect = require("./config/connect");
 const app = express();
 const PORT = process.env.PORT;
 
-
 const userRoute = require("./user/user.route")
 const productRoute = require("./product/product.route")
 const cartRoute = require("./cart/cart.route")
 
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json())
 
 app.use('/user', userRoute)
--- backend/src/user/user.route.js
+++ backend/src/user/user.route.js
@@ -2,105 +2,106 @@ const { Router } = require("express");
 const jwt = require("jsonwebtoken");
 const OtpModel = require("../otp/otp.model");
 const userModel = require("./user.model");
-const nodemailer = require("nodemailer")
-const app = Router()
-
+const nodemailer = require("nodemailer");
+const app = Router();
 
 const emailuser = process.env.EMAILUSERNAME;
 const emailpassword = process.env.EMAILPASSWORD;
 
-
-app.get('/', async (req, res) => {
-    try {
-        const users = await userModel.find();
-        res.status(200).send(users);
-    } catch (e) {
-        res.status(400).send(e.message);
-    }
+app.get("/", async (req, res) => {
+  try {
+    const users = await userModel.find();
+    res.status(200).send(users);
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
 });
 
-app.post('/signup', async (req, res) => {
-    try {
-        const { email } = req.body;
-        const findUser = await userModel.findOne({ email: email })
-        if (findUser) {
-            return res.status(200).send({ message: "user already exists" })
-        }
-        const user = await userModel.create(req.body);
-        return res.status(201).send({message: "user created successfully",user});
-    } catch (e) {
-        return res.status(500).send(e.message);
+app.post("/signup", async (req, res) => {
+  try {
+    const { email } = req.body;
+    const findUser = await userModel.findOne({ email: email });
+    if (findUser) {
+      return res.status(200).send({ message: "user already exists" });
     }
+    const user = await userModel.create(req.body);
+    return res.status(201).send({ message: "user created successfully", user });
+  } catch (e) {
+    return res.status(500).send(e.message);
+  }
 });
 
+app.post("/login", async (req, res) => {
+  try {
+    const { email } = req.body;
 
-app.post('/login', async (req, res) => {
-    try {
-        const { email } = req.body;
+    const user = await userModel.findOne({ email: email });
+    if (user) {
+      const verifyemail = await OtpModel.findOne({ email: user.email });
+      if (verifyemail) {
+        return res.send({ message: "Otp already generated" });
+      }
+      const otp = Math.floor(100000 + Math.random() * 900000);
+      const saveOtp = await OtpModel.create({ otp, email });
 
-        const user = await userModel.findOne({ email:email  });
-        if(user){
-            const verifyemail= await OtpModel.findOne({email:user.email})
-            if(verifyemail){
-                return res.send({message:"Otp already generated"})
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: emailuser,
+          pass: emailpassword,
+        },
+      });
 
-            }
-            const otp = Math.floor(100000 + Math.random() * 900000);
-            const saveOtp = await OtpModel.create({ otp, email })    
+      const mailOptions = {
+        from: "pharmeasy@gmail.com",
+        to: email,
+        subject: `OTP generated successfully`,
+        text: `Dear ${user.firstName} the otp to login to your account id ${otp}`,
+      };
 
-            const transporter = nodemailer.createTransport({
-                service: "gmail",
-                auth: {
-                  user: emailuser,
-                  pass: emailpassword,
-                },
-              });
-
-            const mailOptions = {
-                from: 'pharmeasy@gmail.com',
-                to: email,
-                subject: `OTP generated successfully`,
-                text: `Dear ${user.firstName} the otp to login to your account id ${otp}`,
-              };
-          
-              transporter.sendMail(mailOptions, (err, info) => {
-                if (err) {
-                 return console.log("ERROR", err);
-                } else {
-                   console.log("EMAIL SEND" + info.response);
-                   res.send(`OTP SENDED SUCCESSFULLY`);
-                }
-              });
-
-            return res.status(200).send({ message: "OTP has been sent to your email" });
+      transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+          return console.log("ERROR", err);
         } else {
-            res.status(200).send({ message: "You are not registered please register" })
+          console.log("EMAIL SEND" + info.response);
+          res.send(`OTP SENDED SUCCESSFULLY`);
         }
-    } catch (e) {
-        res.status(500).send(e.message);
+      });
+
+      return res
+        .status(200)
+        .send({ message: "OTP has been sent to your email" });
+    } else {
+      res
+        .status(200)
+        .send({ message: "You are not registered please register" });
     }
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
 });
 
+app.post("/verify", async (req, res) => {
+  try {
+    const { email, otp } = req.body;
+    const verify = await OtpModel.findOne({ otp, email });
 
-
-app.post("/verify", async(req,res)=>{
-    try{
-        const { email,otp } = req.body;
-        const verify=await OtpModel.findOne({otp,email})
-       
-        if(!verify){
-           return res.status(404).send("invalid otp")
-        }else{
-            const user = await userModel.findOne({email},{"password": 0})
-            const token = jwt.sign({ id: user._id, email: user.email}, process.env.JWT_SECRET, { expiresIn: '1h' });
-             
-            return res.status(200).send({message:"login success",data:token,user});
-
-
-        }
-
-    } catch {
-        return res.status(400).send("Invalid token");
+    if (!verify) {
+      return res.status(404).send("invalid otp");
+    } else {
+      const user = await userModel.findOne({ email }, { password: 0 });
+      const token = jwt.sign(
+        { id: user._id, email: user.email },
+        process.env.JWT_SECRET,
+        { expiresIn: "1h" }
+      );
+      const deleteotp = await OtpModel.findOneAndDelete({ email });
+      return res
+        .status(200)
+        .send({ message: "login success", data: token, user });
     }
-})
+  } catch {
+    return res.status(400).send("Invalid token");
+  }
+});
 module.exports = app;
